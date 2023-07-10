a8d437d8c7b7e390c80a79c41c5f1aa97c65b1ac
Ldoun <67096173+Ldoun@users.noreply.github.com>
2023-07-10 07:42:35 -0700

    fix: detaching grad

--- trainer.py
+++ trainer.py
@@ -62,7 +62,7 @@ class Trainer():
             loss.backward()
             self.optimizer.step()
             total_loss += loss.item() # *x.shape[0]
-            correct += dice_score(output.logits.cpu().numpy(), mask.cpu().numpy())
+            correct += dice_score(output.logits.detach().cpu().numpy(), mask.detach().cpu().numpy())
         
         return total_loss/self.len_train, correct/self.len_train
     
@@ -83,7 +83,7 @@ class Trainer():
                 
                 loss = output.loss
                 total_loss += loss.item() 
-                correct += dice_score(output.logits.cpu().numpy(), mask.cpu().numpy())
+                correct += dice_score(output.logits.detach().cpu().numpy(), mask.detach().cpu().numpy())
                 
         return total_loss/self.len_valid, correct/self.len_valid
 
