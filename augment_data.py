222d206a77ba2b6bbf5f665cbd5fcda4ea65cf74
Ldoun <67096173+Ldoun@users.noreply.github.com>
2023-07-11 14:24:57 -0700

    fix: augmenting data

--- augment_data.py
+++ augment_data.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 from PIL import Image
 from copy import deepcopy
 import albumentations as A
@@ -9,8 +10,10 @@ import albumentations as A
 from data import rle_decode, rle_encode
 
 def get_720_image(image, mask):
-    images = [], masks = []
+    images = []
+    masks = []
     size = 1024
+
     images.append(deepcopy(image[:720, :720, :]))
     images.append(deepcopy(image[:720, size-720:, :]))
     images.append(deepcopy(image[size-720:, size-720:, :]))
@@ -33,15 +36,18 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    data = pd.read_csv(args.train.csv)
+    data = pd.read_csv(args.train_csv)
     data['img_path'] = data['img_path'].apply(lambda x: os.path.join(args.path, x))
 
     transform = A.RandomResizedCrop(height=224, width=224, ratio=(0.4, 1.6), always_apply=True)
 
-    img_ids = [], mask_rles = [], img_paths = [], has_mask = []
+    img_ids = []
+    mask_rles = []
+    img_paths = []
+    has_mask = []
 
-    for row in data.iterrows():
-        mask = rle_decode(row['mask_rle'])
+    for i, row in tqdm(data.iterrows()):
+        mask = rle_decode(row['mask_rle'], (1024, 1024))
         image = np.array(Image.open(row['img_path']))
         
         images, masks = get_720_image(image, mask)
@@ -62,5 +68,5 @@ if __name__ == "__main__":
     df['mask_rle'] = mask_rles
     df['img_path'] = img_paths
     df['has_mask'] = has_mask
-    
+
     df.to_csv(os.path.join(args.path, "augmented_images.csv"), index=False)
