d6d9147617c45d3eae9e5b21bc7ae880dee2d3dd
Ldoun <67096173+Ldoun@users.noreply.github.com>
2023-07-19 09:00:41 -0700

    fix: dice loss for batched intput

--- dice_loss.py
+++ dice_loss.py
@@ -1,5 +1,5 @@
 import torch
 
 def dice_loss(prediction, target, smooth=1e-7):
-    intersection = torch.sum(prediction * target)
-    return 1 - (2.0 * intersection + smooth) / (torch.sum(prediction) + torch.sum(target) + smooth)
+    intersection = torch.sum(prediction * target, dim=[1,2])
+    return torch.mean(1 - (2.0 * intersection + smooth) / (torch.sum(prediction, dim=[1,2]) + torch.sum(target, dim=[1,2]) + smooth))
